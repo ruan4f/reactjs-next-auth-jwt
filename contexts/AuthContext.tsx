fc328d809bac288a7513c028156444d92bad7807
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-06-27 22:59:35 -0300

    changes

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+}
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  isAuthenticated: boolean;
+}
+
+const AuthContext = createContext({});
