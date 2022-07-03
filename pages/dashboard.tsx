46190d0ecd41920265460bf2a0d999080958e630
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-07-03 15:03:21 -0300

    Create hook permission

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 
@@ -8,19 +9,27 @@ import { withSSRAuth } from "../utils/withSSRAuth";
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
 
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list']
+  });
+
   useEffect(() => {
     api.get('/me').then(response => console.log(response));
   }, []);
 
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
   )
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx);
   const response = await apiClient.get('/me');
-  
+
   return {
     props: {}
   }
